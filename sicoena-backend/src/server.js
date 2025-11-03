5ff7ea939e2e728579aa7e469c33ff04a6c42f7f
Jparedest1 <jhony.paredes1994@gmail.com>
2025-11-03 02:41:28 -0600

    configuracion del server.js

--- sicoena-backend/src/server.js
+++ sicoena-backend/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); 
 const express = require('express');
 const cors = require('cors'); 
+const path = require('path');
 const db = require('./config/db');
 const { startScheduledTasks } = require('./utils/scheduledTasks');
 
@@ -8,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -16,18 +16,20 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('¡API de SICOENA funcionando!');
-});
-
 const mainRoutes = require('./routes/index');
 app.use('/api', mainRoutes); 
 
+app.use(express.static(path.join(__dirname, '../sicoena-frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../sicoena-frontend/build', 'index.html'));
+});
+
 app.use((req, res) => {
-  res.status(404).json({ message: 'Ruta no encontrada en el servidor' });
+  res.status(404).json({ message: 'Ruta no encontrada' });
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
   startScheduledTasks();
 });
